89b36746d9f8fdd0e87165c78a251aa600abb532
mjbam402 <mjbam402@naver.com>
2024-03-19 18:05:51 +0900

    fin?

--- sql_practice_basic.sql
+++ sql_practice_basic.sql
@@ -13,3 +13,18 @@ FROM Purchases WHERE purchase_date BETWEEN
 # 4. 서울에서 거주하는 모든 유저의 이름과 주소를 출력
 SELECT name, address FROM Users
 WHERE address = '서울';
+# 5. 가격이 높은 순서대로 책을 출력합니다.
+SELECT * FROM Books ORDER BY price DESC;
+# 6. 카테고리 이름에 '과학'이 포함된 모든 카테고리를
+# 찾아서 출력하세요.
+SELECT category_id, category_name, category_index
+FROM `Categories` WHERE category_name = '과학';
+# 7. 28세 ~ 30세 사이인 유저의 이메일과 나이를 출력
+SELECT email, age FROM `Users`
+WHERE age BETWEEN 28 AND 30;
+# 8. 매년 출판된 책의 수를 출력하세요.
+# 연도순으로 내림차순 정렬하세요.
+SELECT pub_year as year, count(book_id) as total_books
+FROM Books GROUP BY pub_year 
+ORDER BY total_books DESC;
+# 8번 하던중
